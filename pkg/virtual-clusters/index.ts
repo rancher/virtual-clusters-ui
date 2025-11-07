f538c2520d3bbe63bfe200d6ca2c500c95f597ac
Nancy Butler <42977925+mantis-toboggan-md@users.noreply.github.com>
2025-11-07 15:00:30 -0800

    use findLabelSelector to look for roles; remove call to versions in product configs

--- pkg/virtual-clusters/index.ts
+++ pkg/virtual-clusters/index.ts
@@ -3,7 +3,23 @@ import { IPlugin, ModelExtensionConstructor, PanelLocation } from '@shell/core/t
 import { k3kProvisioner } from './provisioner';
 import { VClusterModelExtension } from './model-extension/provisioning.cattle.io.cluster';
 import virtualClusterRouting from './routes'
+import virtualClusterAdminRole from './resources/virtual-cluster-admin-role.json'
+import virtualClusterPolicyAdminRole from './resources/virtual-cluster-policy-admin-role.json'
+import { MANAGEMENT, SCHEMA } from '@shell/config/types';
+import versions from '@shell/utils/versions';
+import { isRancherPrime } from '@shell/config/version';
 
+const createRoleIfNotFound = async (roleTemplate: any, store:any)=>{
+  const rolesMatching = await store.dispatch('management/findLabelSelector', {type: MANAGEMENT.ROLE_TEMPLATE, matching:  {labelSelector: {matchLabels:roleTemplate.metadata.labels }}}) || []
+
+  if(!rolesMatching.length){
+    const newRole = await store.dispatch('management/create', {type: MANAGEMENT.ROLE_TEMPLATE, ...roleTemplate})
+    const norman = await newRole.norman
+
+    norman.labels = roleTemplate.metadata.labels
+    norman.save()
+  }
+}
 
 
 // Init the package
@@ -29,5 +45,28 @@ export default function(plugin: IPlugin): void {
 
   // Built-in icon
   plugin.metadata.icon = require('./assets/icon-k3k.svg');
-  
+
+
+
+
+  plugin.addNavHooks(undefined, undefined, undefined, async(store: any)=>{
+    try{
+      await versions.fetch({ store: store });
+
+      if(isRancherPrime()){
+        await store.dispatch('management/loadSchemas')
+        await store.dispatch('rancher/loadSchemas')
+        const roleSchema = store.getters['management/byId'](SCHEMA, MANAGEMENT.ROLE_TEMPLATE )
+
+
+        if((roleSchema?.resourceMethods||[]).find((verb: string) => ['PUT', 'blocked-PUT'].includes(verb))){
+          await store.dispatch('management/findAll', {type: MANAGEMENT.ROLE_TEMPLATE})
+          createRoleIfNotFound(virtualClusterAdminRole, store)
+          createRoleIfNotFound(virtualClusterPolicyAdminRole, store)
+        }
+      }
+    } catch(e) {
+      console.error(e)
+    }
+  })
 }
